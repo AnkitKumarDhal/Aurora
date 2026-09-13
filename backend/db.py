9a871dcceb83990f7c1a41974049af3e2c63975e
Ankit Kumar Dhal <ankitkumardhal2007@gmail.com>
2026-09-13 15:27:05 +0530

    fix: fixed typos in backend/db.py and backend/router/auth.py

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,10 @@
-from motor.motor_asyncio import AsynIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URI, DB_NAME
 
-client = AsynIOMotorClient(MONGO_URI)
+client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
 
 patients_collection = db["patients"]
 sessions_collection = db["sessions"]
-documents_colelction = db["documents"]
+documents_collection = db["documents"]
 summaries_collection = db["summaries"]
--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -15,7 +15,7 @@ class RegisterRequest(BaseModel):
 
 @router.post("/register")
 async def register(req: RegisterRequest):
-    patient_id = str(uuid.uuid4()),
+    patient_id = str(uuid.uuid4())
     login_id = str(uuid.uuid8())[:8],
     patient = {
         "patient_id": patient_id,
@@ -33,7 +33,7 @@ class LoginRequest(BaseModel):
 @router.post("/login")
 async def login(req: LoginRequest):
     patient = await patients_collection.find_one(
-        {"login_id": req.login()},
+        {"login_id": req.login_id},
         {"_id": 0}
     )
     if not patient:
