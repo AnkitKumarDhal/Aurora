757daf68c2f9de714b128234f6d701ba6fb9acf7
Ankit Kumar Dhal <ankitkumardhal2007@gmail.com>
2026-09-15 20:39:49 +0530

    fixed dependency api

--- backend/api/dependencies.py
+++ backend/api/dependencies.py
@@ -1,19 +1,7 @@
-from collections.abc import AsyncGenerator
-from fastapi import Depends
-from pymongo.asynchronous.database import AsyncDatabase
-
-from backend.database import get_database
 from backend.database.repositories.clinical_session import ClinicalSessionRepository
-from backend.database.repositories.queue import QueueRepository
 from backend.services.clinical_session import ClinicalSessionService
-from backend.services.queue import QueueService
 
 
-async def get_clinical_session_service(database: AsyncDatabase = Depends(get_database)) -> AsyncGenerator[ClinicalSessionService, None]:
-    repository = ClinicalSessionRepository(database)
-    yield ClinicalSessionService(repository)
-
-
-async def get_queue_service(database: AsyncDatabase = Depends(get_database)) -> AsyncGenerator[QueueService, None]:
-    repository = QueueRepository(database)
-    yield QueueService(repository)
+def get_clinical_session_service() -> ClinicalSessionService:
+    repository = ClinicalSessionRepository()
+    return ClinicalSessionService(repository)
